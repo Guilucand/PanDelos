99fe727609e9507bf9cabfc05260ca64133150b8
Andrea Cracco <guilucand@gmail.com>
2020-01-12 21:05:44 +0100

    Fix native headers and compile script

--- ig/compile.sh
+++ ig/compile.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-mkdir native/build
+mkdir -q native/build
 cd native/build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
 make -j
 cd ../../
-javac -classpath ext/commons-io-2.6.jar  -sourcepath ./ infoasys/cli/pangenes/Pangenes.java & jar cvf ig.jar infoasys/
+
+javac -classpath ext/commons-io-2.6.jar  -sourcepath ./ infoasys/cli/pangenes/Pangenes.java && jar cvf ig.jar infoasys/
+rm $(find -name '*.class')
+
+
