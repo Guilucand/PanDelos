0e429354117c0060a94e1c2433fb43f3b12e4292
Andrea Cracco <guilucand@gmail.com>
2020-01-12 21:13:09 +0100

    Fix download script to download a file only if is non-existent or has size zero

--- example/download.sh
+++ example/download.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
+
+if [ $# -le 1 ]; then
+	echo "Not enough arguments! [ilist, prefix] required";
+	exit
+fi
+
 ilist="$1"
 prefix="$2"
+
 mkdir -p $prefix
-rm $prefix/*.gbk
+crtdir=$PWD
+
+cd $prefix
+find -name '*.gbk' -size  0 -print0 | xargs -0 rm --
+cd $crtdir
+
 while read line
 do
 echo $line
 id=`echo $line | sed s/^.*\_NC\_/NC\_/g`
 id=`echo $id | sed s/^.*\_NZ\_/NZ\_/g`
-echo "efecth for $id"
-efetch -db nuccore -id $id -format gbwithparts > ${prefix}/${id}.gbk
+echo "efetch for $id"
+if [ ! -f "${prefix}/${id}.gbk" ]; then
+	efetch -db nuccore -id $id -format gbwithparts > ${prefix}/${id}.gbk;
+fi
 done < "$ilist"
 python3 gbk2ig.py ${prefix}/ ${prefix}.faa
